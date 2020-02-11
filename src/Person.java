fce5d12a61751ad2a0e5f8e3e8fcaa8b89eecd22
ainars.galvans@ctco.lv <MiluKodet1>
2020-02-11 16:27:52 +0200

    Person with name and surname

--- src/Person.java
+++ src/Person.java
@@ -3,7 +3,10 @@ import java.util.UUID;
 public class Person {
     public String firstName;
     public String lastName;
-
+    public Person(String firstname, String lastName) {
+        this.firstName=firstname;
+        this.lastName=lastName;
+    }
     public String getFirstName() {
         return firstName;
     }
